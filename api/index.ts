f5bedca5b3fe94c0cf089455d5f00f19f589a06a
ParsaProg <parsashaabani3@gmail.com>
2025-09-09 19:57:53 +0330

    remove hono-serve

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,10 @@
-import {Hono} from "hono"
-import { serve } from '@hono/node-server'
+import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/messages", (C) => C.text("Api is running "))
+app.get("/messages", (C) => C.text("Api is running "));
 
 app.post("/messages", async (message) => {
-    const body = await message.req.json();
-    return message.json({"success": true, "data": body})
-})
-
-serve({
-    fetch: app.fetch,
-    port: 3001
-})
+  const body = await message.req.json();
+  return message.json({ success: true, data: body });
+});
