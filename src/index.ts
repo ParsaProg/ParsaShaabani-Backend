01e5baa53af9921f05b306efe84cd13090b9154d
ParsaProg <parsashaabani3@gmail.com>
2025-09-09 22:38:32 +0330

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { Hono } from "hono"
 
 const app = new Hono()
 
-app.get("/messages", (c) => c.text("API is running"))
+app.get("/", (c) => c.text("API is running"));
+
+app.get("/messages", (c) => c.json({"Messages": "nothing"}));
 
 app.post("/messages", async (c) => {
     const body = await c.req.json()
