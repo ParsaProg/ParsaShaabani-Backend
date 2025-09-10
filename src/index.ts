f9340de7429a8d065cf26c7c4d73e9ff67fc4632
ParsaProg <parsashaabani3@gmail.com>
2025-09-10 13:13:24 +0330

    changes for the schema and real user data

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
 import { Hono } from "hono";
 
+interface User{
+    name: string;
+    age: number;
+}
+
 const app = new Hono();
 
-app.get("/messages", (c) => c.text("API is running"));
+const myData: User[] = [
+    {
+        name: "Parsa",
+        age: 16
+    },
+    {
+        name: "Yasna",
+        age: 19
+    }
+]
+app.get("/messages", (c) => c.json(myData));
 
 app.post("/messages", async (c) => {
     try {
         const body = await c.req.json();
+        myData.push(body);
         return c.json({ success: true, data: body });
     } catch (error) {
         return c.json({ success: false, error: "Invalid request" }, 400);
