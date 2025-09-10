74e16fe072f30f1ba91b638f19675ac278bb33d1
ParsaProg <parsashaabani3@gmail.com>
2025-09-10 14:45:52 +0330

    chnage for fix the build errors

---
+++ dist/index.js
@@ -0,0 +1,29 @@
+import { Hono } from "hono";
+import { prisma } from "./lib/prisma.js";
+const app = new Hono();
+app.get("/users", async (c) => {
+    const users = await prisma.user.findMany({
+        orderBy: { createdAt: "desc" },
+    });
+    return c.json(users);
+});
+app.post("/users", async (c) => {
+    try {
+        const body = await c.req.json();
+        if (!body.name || !body.age) {
+            return c.json({ success: false, error: "name and age required" }, 400);
+        }
+        const user = await prisma.user.create({
+            data: {
+                name: body.name,
+                age: body.age,
+            },
+        });
+        return c.json({ success: true, data: user });
+    }
+    catch (error) {
+        console.error(error);
+        return c.json({ success: false, error: "Invalid request" }, 400);
+    }
+});
+export default app;
