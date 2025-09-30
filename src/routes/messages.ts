5a9e61cc87119d78c014774477e37903cad41a73
ParsaProg <parsashaabani3@gmail.com>
2025-09-30 17:05:11 +0330

    add post request handler asycn function to messages route

--- src/routes/messages.ts
+++ src/routes/messages.ts
@@ -10,4 +10,24 @@ messages.get("/", async (c) => {
   return c.json({ messages: messagesData });
 });
 
+messages.post("/", async (c) => {
+  try {
+    const body = await c.req.json<{
+      fullName: string;
+      email: string;
+      Message: string;
+    }>();
+
+    if (!body.fullName || !body.email || !body.Message) {
+      return c.json({ success: false, error: "all fields required" }, 400);
+    }
+
+    const user = await prisma.connectionMessages.create({ data: body });
+    return c.json({ success: true, data: user });
+  } catch (error) {
+    console.error(error);
+    return c.json({ success: false, error: "Invalid request" }, 400);
+  }
+});
+
 export default messages;
